13540a1c062935bddbfd8e4f8c9a2357edb22810
Jose Tejada <jotego@gmail.com>
2020-03-16 09:05:44 +0100

    Added DD1/2 MRA files

---
+++ mist/cpsd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+datestamp="20200312"
+dest=/media/jtejada/3284-6B39
+
+cp 1942/releases/*$datestamp.rbf $dest/JT1942
+cp 1943/releases/*$datestamp.rbf $dest/JT1943
+cp gng/releases/*$datestamp.rbf $dest/JTGNG
+cp tora/releases/*$datestamp.rbf $dest/JTTORA
+cp commando/releases/*$datestamp.rbf $dest/JTCOM
+cp f1dream/releases/*$datestamp.rbf $dest/JTF1DREAM
+cp gunsmoke/releases/*$datestamp.rbf $dest/JTGUN
+cp vulgus/releases/*$datestamp.rbf $dest/JTVULGUS
+cp btiger/releases/*$datestamp.rbf $dest/JTBTIGER
+cp cps1/releases/*$datestamp.rbf $dest/JTCPS1
