667676f2004a9f90149306294e56764ed67cce18
Jose Tejada <jotego@gmail.com>
2020-07-30 16:42:14 +0200

    DD/DD2 minor sound balance adjustment

--- ziptodays.sh
+++ ziptodays.sh
@@ -8,7 +8,7 @@
 # MiSTer RBF is moved into a cores folder inside the zip file
 
 datestr=$(date +"%Y%m%d")
-MRAALT=
+MRAALT=1
 CORE=
 
 function make_zip {
@@ -30,27 +30,43 @@ function make_zip {
                 echo "Error: the 'cores' folder should not exist for this script to work"
                 exit 1
             fi
-            mkdir -p cores            
+            mkdir -p cores
             cp $RBF cores
             zip --recurse-paths $ZIP_NAME cores
             rm -rf cores
+        fi
+        # Add readme file
+        README=../jt$1/README.md
+        if [[ -e $README && -e $ZIP_NAME ]]; then
+            zip --junk-paths $ZIP_NAME $README
+        fi
+        # Add .MRA files
+        if [ "$MRAALT" -eq 1 ]; then
+            mkdir -p temp
+            mv $ZIP_NAME temp/t.zip
+            cp -r ../jt$1/rom/mra/{*.mra,_alt} temp
+            cd temp
+            zip -r t.zip *.mra _alt
+            mv t.zip ../$ZIP_NAME
+            cd ..
+            rm -rf temp
         fi
     fi
 }
 
 function show_help {
     cat << EOF
-Usage: ziptodays.sh corename
+Usage: ziptodays.sh corename [-mra]
+    -nomra      By default the *.mra files in the path ../jt{corename}/rom/mra will be included in the zip
+                as well as _alt subfolder. -nomra disables this behaviour
 EOF
 }
 
 while [ $# -gt 0 ]; do
     case "$1" in
-        -mra)
-            # This is not fully implemented yet
-            shift
-            MRAALT="$1";;
-        -help)
+        -nomra)
+            MRAALT=;;
+        -help|-h|--help)
             show_help
             exit 0;;
         *)
